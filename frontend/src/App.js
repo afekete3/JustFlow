1102fb7eccd54e2d5d463fad0dc1e3e50dc07a34
LukeMedeiros <imlukemedeiros@gmail.com>
2020-01-07 20:08:16 -0500

    added login button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import queryString from 'query-string'
 class App extends React.Component {
 
-  componentWillMount(){
-    fetch('http://localhost:8080')
-    .then(res => {
-      console.log('hello'); 
-      res.json()
-    }); 
-
-  }
 
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <button onClick={()=>window.location='http://localhost:8080/'}> login </button> 
       </div>
     )
   }
+
+  componentDidMount(){
+    let parsed = queryString.parse(window.location.search)
+    let accessToken = parsed['access_token']
+
+  }
+
 }
 export default App

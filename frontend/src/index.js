cd13fde04631e118bf86a3c6f6a8e99391135710
bgibso4 <gibby.b12@gmail.com>
2020-01-10 09:36:30 -0500

    implemented base UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Route, Link, BrowserRouter as Router , Switch} from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 import App from './App';
-import Home from './components/Home'; 
-import * as serviceWorker from './serviceWorker';
 
-const routing = (
-    <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/users" component={Home} />
-      </Switch>
-    </div>
-  </Router>
-  )
+//create the redux store
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 
-ReactDOM.render(routing, document.getElementById('root'));
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />	
+  </Provider>, document.getElementById('root')
+);
+
+// const routing = (
+//     <Router>
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/users">Users</Link>
+//         </li>
+//       </ul>
+//       <Switch>
+//         <Route exact path="/" component={App} />
+//         <Route path="/users" component={Home} />
+//       </Switch>
+//     </div>
+//   </Router>
+//   )
+
+// ReactDOM.render(routing, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();

bbe32b8180a39f9559b64b94842792e9c00847f4
Anthony Fekete <afekete3@uwo.ca>
2020-01-13 00:15:31 -0500

    Updated script to download entire spotify playlists

--- preview_downloader/downloader.py
+++ preview_downloader/downloader.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import processor
 import os
+import json
 from pathlib import Path
 from pymongo import MongoClient
 
@@ -30,23 +31,39 @@ def delete_file(path):
 client = MongoClient("mongodb+srv://JustFlowAdmin:capstone123!@justflow-l8dim.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database('track_db')
 
+# This needs to be updated a lot. Get a new one here: https://developer.spotify.com/console/get-playlist/?playlist_id=37i9dQZF1DXarRysLJmuju&market=&fields=
+auth_token = 'BQDEY-nzM8KEMQWd1s5hgoQ712y3l4pEvADBF0pFIMQXQvv7zhTiU5owtDnfEuFmj4x-XnYJd_Gpke5cGbAMG2nZi9ceXzjSZlJYFQdIU_uQ-w7QIFQyMBBXKpqee6nU4fOMG1tLDTFQcL5oBYumuo_Kb9ldJdOJAu4GcvS6Iau0'
+playlist_url = 'https://api.spotify.com/v1/playlists/37i9dQZF1DXarRysLJmuju/tracks'
+response = requests.get(playlist_url, headers={'Authorization': 'Bearer ' + auth_token}) 
+playlist = json.loads(response.content)
 records = db.track_info
-song_name = sys.argv[2]
-url = sys.argv[1]
-path = download_preview(url, song_name)
-mfcc = processor.get_flattened_mfcc(file = path, name = song_name)
+for item in playlist['items']:
+    track = item['track']
+    if track['preview_url'] is None:
+        print(track['name'])
+        continue
+    song_name = track['name']
+    track_id = track['id']
+    preview_url = track['preview_url']
+    path = download_preview(preview_url, song_name)
+    mfcc = processor.get_flattened_mfcc(file = path, name = song_name)
 
-# storing a byte array of the mp3 file. 
-track_bytes = get_bytes_from_file(path)
+    # storing a byte array of the mp3 file. 
+    track_bytes = get_bytes_from_file(path)
 
-new_track = {
-    'url' : url,
-    'name' : song_name,
-    'mfcc' : mfcc.tolist(),
-    'preview' : track_bytes
-}
+    new_track = {
+        'track_id' : track_id,
+        'preview_url' : preview_url,
+        'name' : song_name,
+        'mfcc' : mfcc.tolist(),
+        'preview' : track_bytes
+    }
 
-records.insert_one(new_track)
-delete_file(path)
+    records.insert_one(new_track)
+    delete_file(path)
+
+
+
+
 
 

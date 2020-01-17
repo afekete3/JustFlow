2f83c3a59d05a681f49c365596d50c28ded0c12f
Anthony Fekete <afekete3@uwo.ca>
2020-01-17 11:02:42 -0500

    More work on playlist script

--- preview_downloader/downloader.py
+++ preview_downloader/downloader.py
@@ -5,21 +5,20 @@ import os
 import json
 from pathlib import Path
 from pymongo import MongoClient
+import librosa
 import spotipy
 import spotipy.util as util
 import spotipy.oauth2 as oauth2
 
-
-def download_preview(url, name):
+LOCAL_FILENAME = 'preview.mp3'
+def download_preview(url):
     # Creates the file in the working directory
-    local_filename = name + '.mp3'
     spotify_preview = requests.get(url)   
-    downloaded_preview = open(local_filename, 'wb')
+    downloaded_preview = open(LOCAL_FILENAME, 'wb')
     for chunk in spotify_preview.iter_content(chunk_size=512 * 1024): 
         if chunk: # filter out keep-alive new chunks
             downloaded_preview.write(chunk)
     downloaded_preview.close()
-    return local_filename
 
 def get_bytes_from_file(filename):
     output = []
@@ -32,7 +31,6 @@ def delete_file(path):
 
 
 # start
-
 client = MongoClient("mongodb+srv://JustFlowAdmin:capstone123!@justflow-l8dim.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database('JustFlow')
 
@@ -46,42 +44,49 @@ auth = oauth2.SpotifyClientCredentials(
     client_secret=CLIENT_SECRET
 )
 
+
 token = auth.get_access_token()
-playlist_url = 'https://api.spotify.com/v1/playlists/37i9dQZF1DXarRysLJmuju/tracks'
-response = requests.get(playlist_url, headers={'Authorization': 'Bearer ' + token}) 
-playlist = json.loads(response.content)
 tracks = db.tracks
 
-for item in playlist['items']:
-    track = item['track']
-    track_id = track['id']
-    if tracks.find({'_id': track_id}) != 'null':
-        break
-    if track['preview_url'] is None:
-        print(track['name'])
-        continue
-    song_name = track['name']
-    
-    preview_url = track['preview_url']
-    path = download_preview(preview_url, song_name)
-    mfcc = processor.get_flattened_mfcc(file = path)
-    chroma = processor.get_chroma_features(file = path)
+with open('playlists.json', 'r') as file: 
+    playlists = json.load(file)
 
-    # storing a byte array of the mp3 file. 
-    track_bytes = get_bytes_from_file(path)
-    print(track_id)
+for playlist_id in playlists['playlists']:
+    playlist_url = 'https://api.spotify.com/v1/playlists/' + playlist_id + '/tracks'
+    response = requests.get(playlist_url, headers={'Authorization': 'Bearer ' + token}) 
+    playlist = json.loads(response.content)
+    for item in playlist['items']:
+        track = item['track']       
+        track_id = track['id']  
+          
+        if tracks.count_documents({'_id': track_id}) > 0:
+            print(track_id)
+            continue
+        if track['preview_url'] is None:
+            print(track['name'])
+            continue
+        
+        song_name = track['name']
+        preview_url = track['preview_url']
+        download_preview(preview_url)
+        y, sr = librosa.load(LOCAL_FILENAME, duration=10.0)
+        mfcc = processor.get_flattened_mfcc(y=y, sr=sr)
+        chroma = processor.get_chroma_features(y=y, sr=sr)
+        tempo = processor.get_tempo(y=y, sr=sr)
 
-    new_track = {
-        '_id' : track_id,
-        'preview_url' : preview_url,
-        'name' : song_name,
-        'mfcc' : mfcc.tolist(),
-        'chroma' : chroma.tolist(),
-        'preview' : track_bytes
-    }
-    tracks.insert_one(new_track)
-    delete_file(path)
-    break
+        # storing a byte array of the mp3 file. 
+        track_bytes = get_bytes_from_file(LOCAL_FILENAME)
+        new_track = {
+            '_id' : track_id,
+            'preview_url' : preview_url,
+            'name' : song_name,
+            'mfcc' : mfcc.tolist(),
+            'chroma' : chroma.tolist(),
+            'tempo': tempo,
+            'preview' : track_bytes
+        }
+        tracks.insert_one(new_track)
+        delete_file(LOCAL_FILENAME)
 
 
 
--- preview_downloader/processor.py
+++ preview_downloader/processor.py
@@ -2,15 +2,13 @@ from pathlib import Path
 import librosa, librosa.display
 import numpy, scipy, matplotlib.pyplot as plt, sklearn, librosa, mir_eval, IPython.display, urllib
 
-def get_flattened_mfcc(file):
-    y, sr = librosa.load(file, duration=10.0)
-    mfcc = librosa.feature.mfcc(y, sr=sr).flatten()
-    return mfcc
+def get_flattened_mfcc(y, sr):
+    return librosa.feature.mfcc(y, sr=sr).flatten()
 
-def get_chroma_features(file):
-    y, sr = librosa.load(file, duration=10.0)
-    chroma = librosa.feature.chroma_stft(y=y, sr=sr).flatten()
-    print(chroma)
-    return chroma
+def get_chroma_features(y, sr):
+    return librosa.feature.chroma_stft(y=y, sr=sr).flatten()
+
+def get_tempo(y, sr):
+    return  round(librosa.beat.beat_track(y, sr=sr, units='time')[0])
 
 

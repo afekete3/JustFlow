1e68131f4955ba1c9a9bb678bf8170c512020042
Luke Medeiros <imlukemedeiros@gmail.com>
2020-02-11 16:21:13 -0500

    Updates

---
+++ precache-manifest.72797d0074545d498ba5b242f24b57d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e787a463d7429b20c1ba4b4efea1c8",
+    "url": "/JustFlow/index.html"
+  },
+  {
+    "revision": "5c7d2ebd34ef3e5c7b1c",
+    "url": "/JustFlow/static/css/main.78d549dc.chunk.css"
+  },
+  {
+    "revision": "cafe77fad88116631b92",
+    "url": "/JustFlow/static/js/2.b684f297.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/JustFlow/static/js/2.b684f297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c7d2ebd34ef3e5c7b1c",
+    "url": "/JustFlow/static/js/main.96069e87.chunk.js"
+  },
+  {
+    "revision": "3008501b4dbb585acad8",
+    "url": "/JustFlow/static/js/runtime-main.20108957.js"
+  }
+]);

0417fc12008d6cd208a4a9029faa95e2b716ff66
Luke Medeiros <imlukemedeiros@gmail.com>
2020-02-11 16:10:31 -0500

    removing spotify preview downloader

--- preview_downloader/tests/youtube_downloader_tests.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append(".")
-import youtube_downloader
-
-youtube_downloader = youtube_downloader.YoutubeDownloader()
-
-url = youtube_downloader.search("The Way", ["CARSTN"])
-youtube_downloader.download_song(url)
